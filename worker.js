3c227c0558ca24558c93a598ae1199a49c42619b
korshev <16843757+Korshev@users.noreply.github.com>
2018-11-13 08:13:27 -0700

    add xlsx to worker

--- worker.js
+++ worker.js
@@ -1,7 +1,23 @@
 importScripts("https://unpkg.com/xlsx/dist/xlsx.full.min.js");
 
-self.addEventListener('message', function(e) {
-    var message = e.data.name + ' to myself!';
-    self.postMessage(message);
-    self.close();
-  })
+self.addEventListener('message', function (e) {
+    var file = e.data;
+    var message = 'Worker received: ' + file.name;
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var data = e.target.result;
+        var workbook = XLSX.read(data, { type: 'binary' });
+
+        var sheets = workbook.SheetNames;
+        var sheet = workbook.Sheets[sheets[0]];
+        var json = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        self.postMessage(json);
+        self.close();
+    }
+    reader.readAsBinaryString(file);
+
+    // self.postMessage(message);
+    // self.close();
+})
